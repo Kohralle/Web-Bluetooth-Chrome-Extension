400d4d1d1ebcd695a232751ba205f03b3cbd2442
karol.komorniczak@gmail.com <komorniczak.karol@gmail.com>
2020-07-18 15:28:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+function init_connect() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "connect"});
+    });
+}
+
+function read() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "read"});
+    });
+}
+
+function stop() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "stop"});
+    });
+}
+/*
+function train() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "train"});
+    });
+}
+*/
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("connect").addEventListener("click", init_connect);
+    document.getElementById("emit_values").addEventListener("click", read);
+    document.getElementById("stop_values").addEventListener("click", stop);
+    //document.getElementById("train").addEventListener("click", train);
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    document.getElementById("p1").innerHTML = message.quaternion.x;
+    document.getElementById("p2").innerHTML = message.quaternion.y;
+    document.getElementById("p3").innerHTML = message.quaternion.z;
+
+    sendResponse({
+        data: "I am fine, thank you. How is life in the background?"
+    });
+});
